e8bf17141ce3a1a2186e00d1543a8ac1a683d668
PC-201804281639\Administrator <791869610@qq.com>
2018-05-14 00:27:13 +0800

    路由

---
+++ spring_cloud/spring_cloud_zuul/src/main/java/com/spring_cloud_zuul/spring_cloud_zuul/SpringCloudZuulApplication.java
@@ -0,0 +1,14 @@
+package com.spring_cloud_zuul.spring_cloud_zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy//开启zuul功能
+@SpringBootApplication
+public class SpringCloudZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZuulApplication.class, args);
+	}
+}
